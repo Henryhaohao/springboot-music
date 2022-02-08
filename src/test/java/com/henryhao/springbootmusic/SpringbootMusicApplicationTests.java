894be32ee23e32e8cdee5ff55c06094d426eb30d
henry <lijh@suosi.com.cn>
2022-02-08 11:39:02 +0800

    sprintboot-music project

---
+++ src/test/java/com/henryhao/springbootmusic/SpringbootMusicApplicationTests.java
@@ -0,0 +1,14 @@
+package com.henryhao.springbootmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
